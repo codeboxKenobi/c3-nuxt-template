c2d0b3fd72fb4fefc8076e7e6e1bdce83d656e0f
codeboxKenobi <kenobi.box@gmail.com>
2022-03-03 21:53:25 +0300

    first nuxt-c3 template

---
+++ components/ChartLine.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="chart" id="chart"></div>
+</template>
+
+<script>
+
+    export default {
+
+        data() {
+            return {
+                
+            }
+        },
+
+        mounted() {
+            this.chart = this.$c3.generate({
+                bindto: '#chart',
+                    data: {
+                    columns: [
+                        ['data1', 30, 200, 100, 400, 150, 250, 20],
+                        ['data2', 50, 20, 10, 40, 15, 25, 120]
+                    ],
+                },
+            })
+            this.addData()
+        },
+
+        methods: {
+            addData() {
+                setTimeout(() => {
+                    this.chart.load({
+                        columns: [
+                            ['data3', 300, 100, 250, 150, 300, 150, 500],
+                            ['data4', 100, 200, 150, 50, 100, 250, 40]
+                        ]
+                    });
+                }, 2000)
+            }
+        }
+    }
+</script>
+
+<style lang="css" scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <ChartLine />
+  </div>
+</template>
+
+<script>
+import ChartLine from '../components/ChartLine.vue'
+export default {
+  name: 'IndexPage',
+  components: {
+    ChartLine
+  }
+}
+</script>
